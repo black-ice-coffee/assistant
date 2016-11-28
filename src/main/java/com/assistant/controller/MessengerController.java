03c6426e1c25012f778e0ff6e48446725b5d27dd
Truc Thai <nguyentrucdn@gmail.com>
2016-11-28 20:58:56 +0700

    - webhook

--- src/main/java/com/assistant/controller/MessengerController.java
+++ src/main/java/com/assistant/controller/MessengerController.java
@@ -1,6 +1,7 @@
 package com.assistant.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,16 @@ public class MessengerController {
     public String hi(){
         return "hello world";
     }
+
+    @RequestMapping("webhook")
+    public String webhook(@RequestParam(value="hub.verify_token", defaultValue="") String token,
+                          @RequestParam(value="hub.challenge", defaultValue="") String challenge){
+
+        if("my_voice_is_my_password_verify_me".equalsIgnoreCase(token)){
+            return challenge;
+        }
+        return "error";
+    }
+
 
 }
